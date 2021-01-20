db27b7345f3cba44c5afc8b881662ce2c9ef944f
Yaqub Aliyev <yaqub00e4513@gmail.com>
2021-01-20 19:38:43 +0400

    spring project created

---
+++ projects/bookshopping_yaqub_e/src/main/java/az/developia/bookshopping_yaqub_e/BookshoppingYaqubEApplication.java
@@ -0,0 +1,13 @@
+package az.developia.bookshopping_yaqub_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshoppingYaqubEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshoppingYaqubEApplication.class, args);
+	}
+
+}
---
+++ projects/bookshopping_yaqub_e/src/test/java/az/developia/bookshopping_yaqub_e/BookshoppingYaqubEApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.bookshopping_yaqub_e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshoppingYaqubEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

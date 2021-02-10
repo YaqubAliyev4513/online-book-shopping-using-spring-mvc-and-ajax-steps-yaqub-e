bf25ff3bf4f0effb38cad0a3271b700e44684103
Yaqub Aliyev <yaqub00e4513@gmail.com>
2021-02-10 19:36:37 +0400

    file upload step 1

--- projects/bookshopping_yaqub_e/src/main/java/az/developia/bookshopping_yaqub_e/BookshoppingYaqubEApplication.java
+++ projects/bookshopping_yaqub_e/src/main/java/az/developia/bookshopping_yaqub_e/BookshoppingYaqubEApplication.java
@@ -1,13 +1,25 @@
 package az.developia.bookshopping_yaqub_e;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import az.developia.bookshopping_yaqub_e.file.StorageService;
 
 @SpringBootApplication
 public class BookshoppingYaqubEApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookshoppingYaqubEApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
---
+++ projects/bookshopping_yaqub_e/src/main/java/az/developia/bookshopping_yaqub_e/file/StorageException.java
@@ -0,0 +1,12 @@
+package az.developia.bookshopping_yaqub_e.file;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
